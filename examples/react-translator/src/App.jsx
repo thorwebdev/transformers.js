a11e8097e7571b355adf70fb0016a4d4cb538e9c
thorwebdev <thor@supabase.io>
2024-07-25 18:56:18 +0800

    feat: whsiper broadcast.

--- examples/react-translator/src/App.jsx
+++ examples/react-translator/src/App.jsx
@@ -1,20 +1,28 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react';
 import LanguageSelector from './components/LanguageSelector';
 import Progress from './components/Progress';
+import { createClient } from '@supabase/supabase-js';
 
-import './App.css'
+const SUPABASE_URL = 'https://buktbeyvzlhbalkpodkn.supabase.co';
+const SUPABASE_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1a3RiZXl2emxoYmFsa3BvZGtuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE4ODk5MzgsImV4cCI6MjAzNzQ2NTkzOH0.zHIazuMNLhXUlYjplCRTIZIoj1wAUpocCx_2-zMyP1I';
+
+import './App.css';
+import { LANGUAGES, languageMapping } from './utils/languages';
 
 function App() {
-
   // Model loading
   const [ready, setReady] = useState(null);
-  const [disabled, setDisabled] = useState(false);
+  const disabled = useRef(false);
   const [progressItems, setProgressItems] = useState([]);
 
   // Inputs and outputs
-  const [input, setInput] = useState('I love walking my dog.');
-  const [sourceLanguage, setSourceLanguage] = useState('eng_Latn');
-  const [targetLanguage, setTargetLanguage] = useState('fra_Latn');
+  const [input, setInput] = useState('Hallo.');
+  const inputRef = useRef(input);
+  const [sourceLanguage, setSourceLanguage] = useState('deu_Latn');
+  const sourceLanguageRef = useRef(sourceLanguage);
+  const [targetLanguage, setTargetLanguage] = useState('eng_Latn');
+  const targetLanguageRef = useRef(targetLanguage);
   const [output, setOutput] = useState('');
 
   // Create a reference to the worker object.
@@ -25,7 +33,7 @@ function App() {
     if (!worker.current) {
       // Create the worker if it does not yet exist.
       worker.current = new Worker(new URL('./worker.js', import.meta.url), {
-        type: 'module'
+        type: 'module',
       });
     }
 
@@ -35,15 +43,15 @@ function App() {
         case 'initiate':
           // Model file start load: add a new progress item to the list.
           setReady(false);
-          setProgressItems(prev => [...prev, e.data]);
+          setProgressItems((prev) => [...prev, e.data]);
           break;
 
         case 'progress':
           // Model file progress: update one of the progress items.
-          setProgressItems(
-            prev => prev.map(item => {
+          setProgressItems((prev) =>
+            prev.map((item) => {
               if (item.file === e.data.file) {
-                return { ...item, progress: e.data.progress }
+                return { ...item, progress: e.data.progress };
               }
               return item;
             })
@@ -52,8 +60,8 @@ function App() {
 
         case 'done':
           // Model file loaded: remove the progress item from the list.
-          setProgressItems(
-            prev => prev.filter(item => item.file !== e.data.file)
+          setProgressItems((prev) =>
+            prev.filter((item) => item.file !== e.data.file)
           );
           break;
 
@@ -69,7 +77,7 @@ function App() {
 
         case 'complete':
           // Generation complete: re-enable the "Translate" button
-          setDisabled(false);
+          disabled.current = false;
           break;
       }
     };
@@ -78,49 +86,89 @@ function App() {
     worker.current.addEventListener('message', onMessageReceived);
 
     // Define a cleanup function for when the component is unmounted.
-    return () => worker.current.removeEventListener('message', onMessageReceived);
+    return () =>
+      worker.current.removeEventListener('message', onMessageReceived);
   });
 
   const translate = () => {
-    setDisabled(true);
+    if (disabled.current) return;
+    if (sourceLanguageRef.current === targetLanguageRef.current) {
+      setOutput(inputRef.current);
+      return;
+    }
+    disabled.current = true;
+    console.log('Translating...');
     worker.current.postMessage({
-      text: input,
-      src_lang: sourceLanguage,
-      tgt_lang: targetLanguage,
+      text: inputRef.current,
+      src_lang: sourceLanguageRef.current,
+      tgt_lang: targetLanguageRef.current,
     });
-  }
+  };
+
+  // Start on load
+  useEffect(() => {
+    translate();
+    // Subscribe to Supabase realtime broadcast
+    const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+    const channel = supabase.channel('cityjsconfsg');
+    channel
+      .on('broadcast', { event: 'transcript' }, ({ payload }) => {
+        setInput(payload.message);
+        inputRef.current = payload.message;
+        setSourceLanguage(languageMapping[payload.language]);
+        sourceLanguageRef.current = languageMapping[payload.language];
+        translate();
+      })
+      .subscribe();
+  }, []);
 
   return (
     <>
       <h1>Transformers.js</h1>
       <h2>ML-powered multilingual translation in React!</h2>
 
-      <div className='container'>
-        <div className='language-container'>
-          <LanguageSelector type={"Source"} defaultLanguage={"eng_Latn"} onChange={x => setSourceLanguage(x.target.value)} />
-          <LanguageSelector type={"Target"} defaultLanguage={"fra_Latn"} onChange={x => setTargetLanguage(x.target.value)} />
+      <div className="container">
+        <div className="textbox-container">
+          <h3>
+            Transcript:{' '}
+            {
+              Object.entries(LANGUAGES).find(
+                ([key, val]) => val === sourceLanguage
+              )?.[0]
+            }
+          </h3>
         </div>
 
-        <div className='textbox-container'>
-          <textarea value={input} rows={3} onChange={e => setInput(e.target.value)}></textarea>
+        <div className="textbox-container">
+          <textarea value={input} rows={3} readOnly></textarea>
+        </div>
+
+        <div className="textbox-container">
+          <LanguageSelector
+            type={'Target'}
+            defaultLanguage={targetLanguage}
+            onChange={(x) => {
+              setTargetLanguage(x.target.value);
+              targetLanguageRef.current = x.target.value;
+            }}
+          />
+        </div>
+
+        <div className="textbox-container">
           <textarea value={output} rows={3} readOnly></textarea>
         </div>
       </div>
 
-      <button disabled={disabled} onClick={translate}>Translate</button>
-
-      <div className='progress-bars-container'>
-        {ready === false && (
-          <label>Loading models... (only run once)</label>
-        )}
-        {progressItems.map(data => (
+      <div className="progress-bars-container">
+        {ready === false && <label>Loading models... (only run once)</label>}
+        {progressItems.map((data) => (
           <div key={data.file}>
             <Progress text={data.file} percentage={data.progress} />
           </div>
         ))}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- examples/webgpu-whisper/src/App.jsx
+++ examples/webgpu-whisper/src/App.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState, useRef } from 'react';
 import { AudioVisualizer } from './components/AudioVisualizer';
 import Progress from './components/Progress';
 import { LanguageSelector } from './components/LanguageSelector';
+import broadcast from './utils/broadcaster';
 
 const IS_WEBGPU_AVAILABLE = !!navigator.gpu;
 
@@ -11,7 +12,6 @@ const MAX_AUDIO_LENGTH = 30; // seconds
 const MAX_SAMPLES = WHISPER_SAMPLING_RATE * MAX_AUDIO_LENGTH;
 
 function App() {
-
   // Create a reference to the worker object.
   const worker = useRef(null);
 
@@ -26,6 +26,7 @@ function App() {
   const [text, setText] = useState('');
   const [tps, setTps] = useState(null);
   const [language, setLanguage] = useState('en');
+  const languageRef = useRef(language);
 
   // Processing
   const [recording, setRecording] = useState(false);
@@ -39,7 +40,7 @@ function App() {
     if (!worker.current) {
       // Create the worker if it does not yet exist.
       worker.current = new Worker(new URL('./worker.js', import.meta.url), {
-        type: 'module'
+        type: 'module',
       });
     }
 
@@ -53,15 +54,15 @@ function App() {
           break;
 
         case 'initiate':
-          setProgressItems(prev => [...prev, e.data]);
+          setProgressItems((prev) => [...prev, e.data]);
           break;
 
         case 'progress':
           // Model file progress: update one of the progress items.
-          setProgressItems(
-            prev => prev.map(item => {
+          setProgressItems((prev) =>
+            prev.map((item) => {
               if (item.file === e.data.file) {
-                return { ...item, ...e.data }
+                return { ...item, ...e.data };
               }
               return item;
             })
@@ -70,8 +71,8 @@ function App() {
 
         case 'done':
           // Model file loaded: remove the progress item from the list.
-          setProgressItems(
-            prev => prev.filter(item => item.file !== e.data.file)
+          setProgressItems((prev) =>
+            prev.filter((item) => item.file !== e.data.file)
           );
           break;
 
@@ -81,26 +82,32 @@ function App() {
           recorderRef.current?.start();
           break;
 
-        case 'start': {
-          // Start generation
-          setIsProcessing(true);
+        case 'start':
+          {
+            // Start generation
+            setIsProcessing(true);
 
-          // Request new data from the recorder
-          recorderRef.current?.requestData();
-        }
+            // Request new data from the recorder
+            recorderRef.current?.requestData();
+          }
           break;
 
-        case 'update': {
-          // Generation update: update the output text.
-          const { tps } = e.data;
-          setTps(tps);
-        }
+        case 'update':
+          {
+            // Generation update: update the output text.
+            const { tps } = e.data;
+            setTps(tps);
+          }
           break;
 
         case 'complete':
           // Generation complete: re-enable the "Generate" button
           setIsProcessing(false);
           setText(e.data.output);
+          broadcast({
+            message: e.data.output[0],
+            language: languageRef.current,
+          });
           break;
       }
     };
@@ -118,17 +125,20 @@ function App() {
     if (recorderRef.current) return; // Already set
 
     if (navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices.getUserMedia({ audio: true })
-        .then(stream => {
+      navigator.mediaDevices
+        .getUserMedia({ audio: true })
+        .then((stream) => {
           setStream(stream);
 
           recorderRef.current = new MediaRecorder(stream);
-          audioContextRef.current = new AudioContext({ sampleRate: WHISPER_SAMPLING_RATE });
+          audioContextRef.current = new AudioContext({
+            sampleRate: WHISPER_SAMPLING_RATE,
+          });
 
           recorderRef.current.onstart = () => {
             setRecording(true);
             setChunks([]);
-          }
+          };
           recorderRef.current.ondataavailable = (e) => {
             if (e.data.size > 0) {
               setChunks((prev) => [...prev, e.data]);
@@ -143,11 +153,10 @@ function App() {
           recorderRef.current.onstop = () => {
             setRecording(false);
           };
-
         })
-        .catch(err => console.error("The following error occurred: ", err));
+        .catch((err) => console.error('The following error occurred: ', err));
     } else {
-      console.error("getUserMedia not supported on your browser!");
+      console.error('getUserMedia not supported on your browser!');
     }
 
     return () => {
@@ -170,40 +179,75 @@ function App() {
 
       fileReader.onloadend = async () => {
         const arrayBuffer = fileReader.result;
-        const decoded = await audioContextRef.current.decodeAudioData(arrayBuffer);
+        const decoded = await audioContextRef.current.decodeAudioData(
+          arrayBuffer
+        );
         let audio = decoded.getChannelData(0);
-        if (audio.length > MAX_SAMPLES) { // Get last MAX_SAMPLES
+        if (audio.length > MAX_SAMPLES) {
+          // Get last MAX_SAMPLES
           audio = audio.slice(-MAX_SAMPLES);
         }
 
-        worker.current.postMessage({ type: 'generate', data: { audio, language } });
-      }
+        worker.current.postMessage({
+          type: 'generate',
+          data: { audio, language },
+        });
+      };
       fileReader.readAsArrayBuffer(blob);
     } else {
       recorderRef.current?.requestData();
     }
   }, [status, recording, isProcessing, chunks, language]);
 
-  return (
-    IS_WEBGPU_AVAILABLE
-      ? (<div className="flex flex-col h-screen mx-auto justify-end text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-900">
-        {(
-          <div className="h-full overflow-auto scrollbar-thin flex justify-center items-center flex-col relative">
-            <div className="flex flex-col items-center mb-1 max-w-[400px] text-center">
-              <img src="logo.png" width="50%" height="auto" className="block"></img>
-              <h1 className="text-4xl font-bold mb-1">Whisper WebGPU</h1>
-              <h2 className="text-xl font-semibold">Real-time in-browser speech recognition</h2>
-            </div>
+  return IS_WEBGPU_AVAILABLE ? (
+    <div className="flex flex-col h-screen mx-auto justify-end text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-900">
+      {
+        <div className="h-full overflow-auto scrollbar-thin flex justify-center items-center flex-col relative">
+          <div className="flex flex-col items-center mb-1 max-w-[400px] text-center">
+            <img
+              src="logo.png"
+              width="50%"
+              height="auto"
+              className="block"
+            ></img>
+            <h1 className="text-4xl font-bold mb-1">Whisper WebGPU</h1>
+            <h2 className="text-xl font-semibold">
+              Real-time in-browser speech recognition
+            </h2>
+          </div>
 
-            <div className="flex flex-col items-center px-4">
-              {status === null && (<>
+          <div className="flex flex-col items-center px-4">
+            {status === null && (
+              <>
                 <p className="max-w-[480px] mb-4">
                   <br />
-                  You are about to load <a href="https://huggingface.co/onnx-community/whisper-base" target="_blank" rel="noreferrer" className="font-medium underline">whisper-base</a>,
-                  a 73 million parameter speech recognition model that is optimized for inference on the web. Once downloaded, the model (~200&nbsp;MB) will be cached and reused when you revisit the page.<br />
+                  You are about to load{' '}
+                  <a
+                    href="https://huggingface.co/onnx-community/whisper-base"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="font-medium underline"
+                  >
+                    whisper-base
+                  </a>
+                  , a 73 million parameter speech recognition model that is
+                  optimized for inference on the web. Once downloaded, the model
+                  (~200&nbsp;MB) will be cached and reused when you revisit the
+                  page.
                   <br />
-                  Everything runs directly in your browser using <a href="https://huggingface.co/docs/transformers.js" target="_blank" rel="noreferrer" className="underline">🤗&nbsp;Transformers.js</a> and ONNX Runtime Web,
-                  meaning no data is sent to a server. You can even disconnect from the internet after the model has loaded!
+                  <br />
+                  Everything runs directly in your browser using{' '}
+                  <a
+                    href="https://huggingface.co/docs/transformers.js"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="underline"
+                  >
+                    🤗&nbsp;Transformers.js
+                  </a>{' '}
+                  and ONNX Runtime Web, meaning no data is sent to a server. You
+                  can even disconnect from the internet after the model has
+                  loaded!
                 </p>
 
                 <button
@@ -216,42 +260,70 @@ function App() {
                 >
                   Load model
                 </button>
-              </>)}
+              </>
+            )}
 
-              <div className="w-[500px] p-2">
-                <AudioVisualizer className="w-full rounded-lg" stream={stream} />
-                {status === 'ready' && <div className="relative">
-                  <p className="w-full h-[80px] overflow-y-auto overflow-wrap-anywhere border rounded-lg p-2">{text}</p>
-                  {tps && <span className="absolute bottom-0 right-0 px-1">{tps.toFixed(2)} tok/s</span>}
-                </div>}
-
-              </div>
-              {status === 'ready' && <div className='relative w-full flex justify-center'>
-                <LanguageSelector language={language} setLanguage={(e) => {
-                  recorderRef.current?.stop();
-                  setLanguage(e);
-                  recorderRef.current?.start();
-                }} />
-                <button className="border rounded-lg px-2 absolute right-2" onClick={() => {
-                  recorderRef.current?.stop();
-                  recorderRef.current?.start();
-                }}>Reset</button>
-              </div>
-              }
-              {status === 'loading' && (
-                <div className="w-full max-w-[500px] text-left mx-auto p-4">
-                  <p className="text-center">{loadingMessage}</p>
-                  {progressItems.map(({ file, progress, total }, i) => (
-                    <Progress key={i} text={file} percentage={progress} total={total} />
-                  ))}
+            <div className="w-[500px] p-2">
+              <AudioVisualizer className="w-full rounded-lg" stream={stream} />
+              {status === 'ready' && (
+                <div className="relative">
+                  <p className="w-full h-[80px] overflow-y-auto overflow-wrap-anywhere border rounded-lg p-2">
+                    {text}
+                  </p>
+                  {tps && (
+                    <span className="absolute bottom-0 right-0 px-1">
+                      {tps.toFixed(2)} tok/s
+                    </span>
+                  )}
                 </div>
               )}
             </div>
+            {status === 'ready' && (
+              <div className="relative w-full flex justify-center">
+                <LanguageSelector
+                  language={language}
+                  setLanguage={(e) => {
+                    recorderRef.current?.stop();
+                    setLanguage(e);
+                    languageRef.current = e;
+                    recorderRef.current?.start();
+                  }}
+                />
+                <button
+                  className="border rounded-lg px-2 absolute right-2"
+                  onClick={() => {
+                    recorderRef.current?.stop();
+                    recorderRef.current?.start();
+                  }}
+                >
+                  Reset
+                </button>
+              </div>
+            )}
+            {status === 'loading' && (
+              <div className="w-full max-w-[500px] text-left mx-auto p-4">
+                <p className="text-center">{loadingMessage}</p>
+                {progressItems.map(({ file, progress, total }, i) => (
+                  <Progress
+                    key={i}
+                    text={file}
+                    percentage={progress}
+                    total={total}
+                  />
+                ))}
+              </div>
+            )}
           </div>
-        )}
-      </div>)
-      : (<div className="fixed w-screen h-screen bg-black z-10 bg-opacity-[92%] text-white text-2xl font-semibold flex justify-center items-center text-center">WebGPU is not supported<br />by this browser :&#40;</div>)
-  )
+        </div>
+      }
+    </div>
+  ) : (
+    <div className="fixed w-screen h-screen bg-black z-10 bg-opacity-[92%] text-white text-2xl font-semibold flex justify-center items-center text-center">
+      WebGPU is not supported
+      <br />
+      by this browser :&#40;
+    </div>
+  );
 }
 
-export default App
+export default App;
